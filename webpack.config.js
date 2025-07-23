9cd0fee03782234511897dfbfe1ff5af32acad49
janaka120 <janakastill@gmail.com>
2025-07-23 08:22:05 +0800

    webpack.config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,66 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  entry: './src/index.tsx',
+  // switch between production and development modes
+  mode: isProd ? "production" : "development",
+
+  entry: "./src/index.tsx",
+
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    // use content hashes in prod for long-term caching
+    filename: isProd ? "[name].[contenthash:8].js" : "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
+
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: [".tsx", ".ts", ".js"],
   },
+
+  // finer-grained source maps depending on env
+  devtool: isProd ? "source-map" : "eval-cheap-module-source-map",
+
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
+        use: "ts-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.less$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'less-loader'
-        ],
+        use: ["style-loader", "css-loader", "less-loader"],
       },
       {
         test: /\.(png|jpg|gif|ico)$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: { limit: 8192 },
           },
         ],
       },
-    ]
+    ],
   },
+
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'public/index.html',
+      template: "public/index.html",
     }),
   ],
-  devtool: 'source-map',
-  mode: 'development',
+
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'),
+      directory: path.join(__dirname, "public"),
     },
     historyApiFallback: true,
     port: 8080,
     open: true,
-  }
+  },
 };
